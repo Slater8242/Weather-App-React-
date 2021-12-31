e49e114e9835ae5d7ad0ff7ec10a43f65820529b
Slater8242 <46532875+Slater8242@users.noreply.github.com>
2022-01-01 00:51:24 +0200

    App.js Refactoring App.js have been refactored from class to function and I was using the hooks. I loved the hooks because they are much better ,short and understandable to work with

--- src/App.js
+++ src/App.js
@@ -1,115 +1,61 @@
-import {Component} from "react";
+import {useEffect, useState} from "react";
 import Connection from "./services/Connection";
 import ipLocation from "./services/ipLocation";
 import "./styles.scss";
 
-// city.then((item) => item.map((item) => log(item)));
+const App = () => {
+  let [value, setValue] = useState("");
+  let [weather, setWeather] = useState({});
+  let [data, setData] = useState({});
 
-export default class App extends Component {
-  state = {
-    city: null,
-    country: null,
-    temp: null,
-    sunset: null,
-    sunrise: null,
-    snow: null,
-    weather: {
-      icon: null,
-      code: null,
-      desc: null
-    },
-    value: ""
+  const conn = new Connection();
+  const ip = new ipLocation();
+  const icons = "https://www.weatherbit.io/static/img/icons/";
+
+  const dataLoaded = (info) => {
+    setData((data = info));
+    setWeather(info.weather);
   };
-  log = console.log;
 
-  conn = new Connection();
-  ip = new ipLocation();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    conn.dataInfo(value).then((item) => dataLoaded(item));
+  };
 
-  icons = "https://www.weatherbit.io/static/img/icons/";
-
-  componentDidMount() {
-    // this.conn.dataInfo(this.state.value).then((item) => this.dataLoaded(item));
-    // this.updateInfo();
-    // this.test = this.onDataLoaded("baku");
-    // this.dataLoaded();
-    this.ip.ipLocation().then((city) => {
-      this.conn.dataInfo(city).then((item) => this.dataLoaded(item));
+  useEffect(() => {
+    ip.ipLocation().then((city) => {
+      conn.dataInfo(city).then((item) => dataLoaded(item));
     });
-  }
+  }, []);
 
-  updateInfo = () => {
-    const {value} = this.props;
-    this.log(this.props);
-    if (!value) {
-      return;
-    }
-  };
-
-  dataLoaded = (info) => {
-    this.setState({
-      city: info.city,
-      country: info.country,
-      temp: info.temp,
-      feelsLikeTemp: info.feelsLikeTemp,
-      sunset: info.sunset,
-      sunrise: info.sunrise,
-      snow: info.snow,
-      weather: {
-        icon: info.weather.icon,
-        code: info.weather.code,
-        desc: info.weather.description
-      }
-    });
-  };
-
-  handleChange = (event) => {
-    this.setState({value: event.target.value});
-  };
-
-  handleSubmit = (event) => {
-    this.conn.dataInfo(this.state.value).then((item) => this.dataLoaded(item));
-    event.preventDefault();
-  };
-
-  render() {
-    const {
-      city,
-      country,
-      temp,
-      feelsLikeTemp,
-      sunset,
-      sunrise,
-      snow,
-      weather,
-      value
-    } = this.state;
-    return (
-      <>
-        <div>
-          <div id="desc">
-            <img src={`${this.icons}${weather.icon}.png`} alt={weather.desc} />
-            <h1>{temp}°</h1>
-            <h1>{weather.desc}</h1>
-          </div>
-          <h1>
-            {city} {country}
-          </h1>
-          <h1>Feels Like: {feelsLikeTemp}°</h1>
-          <h1>Sunrise: {sunrise} GMT</h1>
-          <h1>Sunset: {sunset} GMT</h1>
-          <h1>Snow: {snow}</h1>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              value={value}
-              onChange={this.handleChange}
-              placeholder="Input city name"
-              required
-            />
-            <input type="submit" value="Submit" />
-          </form>
+  return (
+    <>
+      <div>
+        <div id="desc">
+          <img src={`${icons}${weather.icon}.png`} alt={weather.desc} />
+          <h1>{data.temp}°</h1>
+          <h1>{weather.description}</h1>
         </div>
-      </>
-    );
-  }
-}
+        <h1>
+          {data.city} {data.country}
+        </h1>
+        <h1>Feels Like: {data.feelsLikeTemp}°</h1>
+        <h1>Sunrise: {data.sunrise} GMT</h1>
+        <h1>Sunset: {data.sunset} GMT</h1>
+        <h1>Snow: {data.snow}</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue((value = e.target.value))}
+            placeholder="Input city name"
+            required
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default App;
